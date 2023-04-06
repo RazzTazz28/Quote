3b060f70a8af3fa34070be2ea4c06903ff345a28
RazzTazz28 <103788541+RazzTazz28@users.noreply.github.com>
2023-04-06 15:25:28 +0100

    Update settings.py

--- Fortune/Fortune/settings.py
+++ Fortune/Fortune/settings.py
@@ -19,11 +19,9 @@ BASE_URL = "http://127.0.0.1:8000"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=)l3)sxd)l4z$nx5_3lfqva=ofl2f1r2ht67r94*&9v3^p4f)-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    DEBUG = False
 
 ALLOWED_HOSTS = []
 
