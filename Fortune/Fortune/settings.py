b3d9c5024fcb370e536290057543339bdcaaef94
RazzTazz28 <103788541+RazzTazz28@users.noreply.github.com>
2023-04-07 16:38:45 +0100

    Update settings.py

--- Fortune/Fortune/settings.py
+++ Fortune/Fortune/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'discombobulated.herokuapp.com']
 
@@ -113,6 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # Added manually
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
