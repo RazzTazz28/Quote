b86bd9c68214b70ecc597613224dd0fb4dd1f90b
RazzTazz28 <103788541+RazzTazz28@users.noreply.github.com>
2023-04-07 16:37:40 +0100

    Update settings.py

--- Fortune/Fortune/settings.py
+++ Fortune/Fortune/settings.py
@@ -113,7 +113,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # Added manually
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
